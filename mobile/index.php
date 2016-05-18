fbfd2c0b46c7f3e206ba05f74cd6afadbd34baf1
김재홍 <jjaekong@gmail.com>
2016-05-18 19:15:58 +0900

    모바일 레이아웃 작업 중...

---
+++ mobile/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>사회복지법인 SRC병원</title>
+    <link href="http://fonts.googleapis.com/earlyaccess/notosanskr.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/mobile/css/default.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="main">
+    <div id="wrapper">
+        <nav id="topnav">
+            <ul>
+                <li><a href="#"><img src="/mobile/images/ico_mobile_xs.png" alt="" height="13"> 전화문의</a></li>
+                <li><a href="#"><img src="/mobile/images/ico_calendar_xs.png" alt="" height="13"> 온라인예약</a></li>
+                <li><a href="#"><img src="/mobile/images/ico_mappin_xs.png" alt="" height="13"> 오시는 길</a></li>
+            </ul>
+        </nav>
+        <header id="header">
+            <button type="button" class="btn btn-home"><span class="glyphicon glyphicon-home"></span></button>
+            <h1><img src="/mobile/images/logo.png" alt="사회복지법인 SRC병원" width="80"></h1>
+            <button type="button" class="btn btn-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="sr-only">메뉴버튼</span>
+            </button>
+        </header>
+        <main id="content">
+
+        </main>
+        <footer id="footer">
+            <address>경기도 광주시 초월읍 경수길 25 (지월리 729-6)</address>
+            <dl>
+                <dt>대표명</dt>
+                <dd>민오식</dd>
+                <dt>사업자등록번호</dt>
+                <dd>233-82-00339</dd>
+                <dt>대표번호</dt>
+                <dd>1577-3622</dd>
+                <dt>팩스</dt>
+                <dd>031-799-3720</dd>
+            </dl>
+            <p class="copyright">
+                (C) 2016 SRC재활병원. ALL RIGHT RESERVED.<br>
+                SITE BY <a href="http://www.movementk.com" target="_blank">MOVEMENT K GROUP</a>.
+            </p>
+        </footer>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
