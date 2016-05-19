75e84d5221a63ff13a266064b3d1fc5963ef39c9
kyh5392 <kyh90495392@gmail.com>
2016-05-19 16:21:09 +0900

    모바일: 인사말 작성 중...

---
+++ mobile/introduce/greeting.php
@@ -0,0 +1,44 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/css/introduce.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+	<main id="content" class="introduce greeting">
+		<div class="page-title">
+			<h2>
+				인사말<br>
+				<small>CEO Message</small>
+			</h2>
+		</div>
+		<section class="">
+			<div class="section-header">
+			
+			</div>
+			<div class="section-content">
+			
+			</div>
+		</section>
+		<section class="">
+			<div class="section-header">
+			
+			</div>
+			<div class="section-content">
+			
+			</div>
+		</section>
+		<section class="">
+			<div class="section-header">
+			
+			</div>
+			<div class="section-content">
+			
+			</div>
+		</section>
+	</main>
+<style>
+
+</style>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
