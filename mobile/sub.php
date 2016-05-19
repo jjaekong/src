cdcb8477993b6884dd472a28258dfcc38d30af14
김재홍 <jjaekong@gmail.com>
2016-05-19 15:48:13 +0900

    모바일: 가이드에 섹션헤더 스타일 추가

--- mobile/sub.php
+++ mobile/sub.php
@@ -9,12 +9,19 @@
                 <small>CEO Message</small>
             </h2>
         </div>
-        <div clas="btn-area" style="padding-top: 50px; padding-bottom: 50px;">
-            <p style="text-align: center;">
-                <a href="#" class="btn btn-pink">동의서 다운로드</a>
-                <a href="#" class="btn btn-gray">위임장 다운로드</a>
-                <a href="#" class="btn btn-line">동의서 다운로드</a>
-            </p>
+        <div class="container">
+            <section>
+                <div class="section-header">
+                    <h3>SRC재활병원은 1961년 물리치료실을 개원하여 오늘에 이르기까지 국내 재활의료서비스를 선도하는 대한민국 대표 재활병원으로 운영되고 있습니다.</h3>
+                </div>
+            </section>
+            <div clas="btn-area" style="padding-top: 50px; padding-bottom: 50px;">
+                <p style="text-align: center;">
+                    <a href="#" class="btn btn-pink">동의서 다운로드</a>
+                    <a href="#" class="btn btn-gray">위임장 다운로드</a>
+                    <a href="#" class="btn btn-line">동의서 다운로드</a>
+                </p>
+            </div>
         </div>
     </main>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
