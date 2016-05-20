fd26292ff7aeea90d4f9c892393ab038617e872d
kyh5392 <kyh90495392@gmail.com>
2016-05-20 18:19:57 +0900

    병원소개 > 병원연혁 완료.

--- mobile/introduce/history.php
+++ mobile/introduce/history.php
@@ -3,7 +3,7 @@
 </head>
 <body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
-	<main id="content" class="introduce history">
+	<main id="content" class="introduce yongmun">
 		<div class="page-title">
 			<h2>
 				연혁<br>
@@ -11,81 +11,41 @@
 			</h2>
 		</div>
 		<div class="history-list">
-			<ol>
-				<li class="active"><a href="#">1952 ~<br>1957</a></li>
-				<li><a href="#">1958 ~<br>1972</a></li>
-				<li><a href="#">1973 ~<br>1993</a></li>
-				<li><a href="#">1994 ~<br>2016</a></li>
+			<ol class="tab-menu">
+				<li class="active"><a href="/mobile/introduce/history.php">1952 ~<br>1957</a></li>
+				<li><a href="/mobile/introduce/sindaebang.php">1958 ~<br>1972</a></li>
+				<li><a href="/mobile/introduce/bongcheon.php">1973 ~<br>1993</a></li>
+				<li><a href="/mobile/introduce/gwangju.php">1994 ~<br>2016</a></li>
 			</ol>
 		</div>
-		<section class="bongcheon">
+		<section class="yongmun-dong">
 			<div class="section-header">
 				<img class="img-responsive" src="/mobile/images/introduce/history_img.gif">
 				<div class="container">
-					<h4>봉천동<br><i>설립기</i></h4>
-					<h5>1973 ~ 1993</h5>
+					<h4>용문동<br><i>설립기</i></h4>
+					<h5>1952 ~ 1957</h5>
 				</div>
 			</div>
 			<div class="container">
 				<div class="section-content">
 					<ol>
-						<li><h6>1972. 09. 25</h6><p>신축이전 : 서울특별시 관악구 봉천동 722-3</p></li>
-						<li><h6>1973. 01</h6><p>사회복지시설 최초 자체생산 보장구실 설치, 언어치료실, 임상심리실 개설</p></li>
-						<li><h6>1973. 06</h6><p>재활연구 및 정보소식관련 정보기 ‘케이스스터디’ (추후 ‘재활연구’ 변경) 제1호 발간</p></li>
-						<li><h6>1975. 06</h6><p>소아과 신설</p></li>
-						<li><h6>1976. 03. 12</h6><p>새마을 아동 무료진료센터 개설, 아동진료 실시</p></li>
-						<li><h6>1979. 10. 19</h6><p>제1회 뇌성마비 어머니 교실 개최</p></li>
-						<li><h6>1977. 01. 10</h6><p>뇌성마비병동 증축 준공 : 40병상</p></li>
-						<li><h6>1977. 11. 03</h6><p>제1회 뇌성마비복지대회 개최</p></li>
-						<li><h6>1978. 12</h6><p>보건사회부실시 사회사업기관 종합감사에 최우수 기관 선성 병실증설 : 11실 72병상 증설</p></li>
 						<li>
-							<h6>1978. 12. 15</h6>
-							<p>
-								재활의학과 신설 : 재활의학 전문의 김세주 박사 입사<br>치과진료 시작 : 연세대세브란스병원 지원 주2회 실시<br>병원 명칭 변경 : ‘삼육재활원 부속아동병원’
-							</p>
+							<dl>
+								<dt>1952. 06. 12</dt>
+								<dd>삼육원 설립</dd>
+							</dl>
 						</li>
-						<li><h6>1979. 02. 22</h6><p>제6차 범태평양지역 국제재활대회 발표</p></li>
-						<li><h6>1979. 05. 05</h6><p>대통령 표창 수상 : 민은식 부원장</p></li>
-						<li><h6>1980. 08. 05</h6><p>세계장애인의 해 기념사업추진위원회 / 추진위원 : 민영재 이사장, 실무간사 : 민은식 부원장</p></li>
-						<li><h6>1980. 12</h6><p>사단법인 한국사회사업가협회장 역임 : 민은식 부원장</p></li>
-						<li><h6>1981. 03. 05</h6><p>정인희 박사 재활병원 의료원장 취임</p></li>
-						<li><h6>1981. 04. 20</h6><p>국민포장 수상 : 민은식 부원장</p></li>
-						<li><h6>1982. 06. 11</h6><p>개원 30주년 기념식</p></li>
-						<li><h6>1982. 07. 13</h6><p>보장구 개조수리업 허가 취득(서울특별시)</p></li>
-						<li><h6>1984. 06. 07</h6><p>84 뉴욕 세계장애자올림픽 섭외위원 : 민은식 부원장</p></li>
-						<li><h6>1986. 06. 13</h6><p>제2대 민은식 원장 취임</p></li>
-						<li><h6>1986. 06</h6><p>범올림픽 추진 중앙위원회 중앙위원 : 민은식 원장</p></li>
-						<li><h6>1987. 04. 20</h6><p>국민훈장 동백장 추서 : (故)민은식 원장</p></li>
+						<li>
+							<dl>
+								<dt>1952. 11. 20</dt>
+								<dd>삼육원 이전 : 서울시 용산구 용문동 8-33</dd>
+							</dl>
+						</li>
 					</ol>
 				</div>
 			</div>
 		</section>
 	</main>
-<style>
-/* history */
-.introduce.history .history-list {text-align:center; }
-.introduce.history .history-list ol {overflow:hidden; font-size:12px; width:300px; height:46px; margin:25px 10px; display:inline-block; }
-.introduce.history .history-list ol li {float:left; line-height:16px; text-align:center; color:#666; width:74px; height:46px; padding:5px 0;  
-border-right:1px solid #ccc; border-top:1px solid #ccc; border-bottom:1px solid #ccc; }
-.introduce.history .history-list ol li:first-child {border-left:1px solid #ccc; }
-.introduce.history .history-list ol li:last-child {border-right:1px solid #ccc; }
-.introduce.history .history-list ol li.active {background-color:#444; color:#fff; width:75px; height:46px; border:0; border-left:0 !important; }
-.introduce.history .history-list ol li a {color:#666; width:75px; height:46px; display:inline-block; text-decoration:none; }
-.introduce.history .history-list ol li.active a {color:#fff; width:75px; height:46px; }
-.introduce.history .history-list ol li a:hover {text-decoration:none; }
-.introduce.history .bongcheon .section-header .container h4 {background:none !important; margin-top:30px !important; margin-bottom:0 !important; color:#444 !important; display:block; font-size:20px !important; padding-top:0 !important; } 
-.introduce.history .bongcheon .section-header .container h4 i {font-style:normal; font-size:20px; display:block; padding:8px 0; font-weight:200; } 
-.introduce.history .bongcheon .section-header .container h5 {background:none !important; font-size:13px; display:block; color:#e9193a; padding-top:0 !important; font-weight:200; }
-.introduce.history .bongcheon .container .section-content {margin-bottom:50px; }
-.introduce.history .bongcheon .container .section-content {margin-top:10px; }
-.introduce.history .bongcheon .container .section-content ol {background:url("/mobile/images/ico_section_header.png") 0 0 no-repeat; background-size:20px auto; padding:14px 10px;}
-.introduce.history .bongcheon .container .section-content ol li {border-left:1px solid #ccc; padding-left:10px;  padding-bottom:22px; }
-.introduce.history .bongcheon .container .section-content ol li h6 {color:#e9193a; font-size:12px; margin:0; font-weight:200; }
-.introduce.history .bongcheon .container .section-content ol li:first-child h6 {margin-top:14px; }
-.introduce.history .bongcheon .container .section-content ol li p {color:#666; font-size:12px; margin-top:10px; line-height:21px; }
-.introduce.history .bongcheon .container .section-content ol li:last-child {padding-bottom:0; }
-
-</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
 </body>
