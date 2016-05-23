9a15f1beeea154ab16d9ebd127c9a383430b38ba
김재홍 <jjaekong@gmail.com>
2016-05-23 14:24:06 +0900

    예약완료, 준비중, 스킵네비 추가

---
+++ under_construction.php
@@ -0,0 +1,20 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main id="content">
+        <section class="under-construction">
+            <div class="container">
+                <img src="/images/ico_pc_src.png" alt="">
+                <h1>컨텐츠 준비 중입니다.</h1>
+                <p>
+                    이용에 불편을 드려서 죄송합니다.<br>
+                    빠른 시일내로 정상 운영하도록 하겠습니다.
+                </p>
+            </div>
+        </section>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer_sub.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
