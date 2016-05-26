7cc821230a9c3f352165b17a913467830bacef04
김재홍 <jjaekong@gmail.com>
2016-05-26 12:14:21 +0900

    웹: 예약영역, 의료진영역 배경사이즈 수정, 모바일: 네이게이션 추가

--- mobile/inc/header.php
+++ mobile/inc/header.php
@@ -15,3 +15,93 @@
         <span class="sr-only">메뉴버튼</span>
     </button>
 </header>
+<nav id="nav" class="collapsed">
+    <div class="nav-container">
+        <button type="button" class="btn btn-nav-close"><img src="/mobile/images/btn_close.png" alt="네비게이션 닫기"></button>
+        <ul class="member">
+            <li><a href="#">로그인</a></li>
+            <li><a href="#">회원가입</a></li>
+        </ul>
+        <ul class="quick">
+            <li><a href="#">온라인예약</a></li>
+            <li><a href="#">진료시간 안내</a></li>
+            <li><a href="#">오시는 길</a></li>
+        </ul>
+        <div class="nav-content">
+            <ul class="gnb">
+                <li>
+                    <a href="#">병원소개</a>
+                    <ul>
+                        <li><a href="#">인사말</a></li>
+                        <li><a href="#">병원소개</a></li>
+                        <li><a href="#">연혁</a></li>
+                        <li><a href="#">의료진 소개</a></li>
+                        <li><a href="#">병원 둘러보기</a></li>
+                        <li><a href="#">오시는 길</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">진료과목</a>
+                    <ul>
+                        <li><a href="#">재활의학과</a></li>
+                        <li><a href="#">내과</a></li>
+                        <li><a href="#">가정의학과/소아과</a></li>
+                        <li><a href="#">한의과</a></li>
+                        <li><a href="#">SRC치료법</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">외래(진료센터)</a>
+                    <ul>
+                        <li><a href="#">재활의학과</a></li>
+                        <li><a href="#">내과/가정의학과</a></li>
+                        <li><a href="#">한의과</a></li>
+                        <li><a href="#">언어심리상담센터</a></li>
+                        <li><a href="#">진료안내</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">입원(재활센터)</a>
+                    <ul>
+                        <li><a href="#">뇌졸증 재활센터</a></li>
+                        <li><a href="#">척추손상 재활센터</a></li>
+                        <li><a href="#">호흡 재활센터</a></li>
+                        <li><a href="#">소아 재활센터</a></li>
+                        <li><a href="#">근골격 재활센터</a></li>
+                        <li><a href="#">환자지원 프로그램</a></li>
+                        <li><a href="#">재활병원 입원안내</a></li>
+                        <li><a href="#">VIP병동 입원안내</a></li>
+                        <li><a href="#">입원식단안내</a></li>
+                        <li><a href="#">재활요양 입원안내</a></li>
+                        <li><a href="#">암요양병원(베네라이프)</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">고객서비스</a>
+                    <ul>
+                        <li><a href="#">자주하는 질문</a></li>
+                        <li><a href="#">제증명서 발금안내</a></li>
+                        <li><a href="#">온라인 예약</a></li>
+                        <li><a href="#">부서 전화번호</a></li>
+                        <li><a href="#">비급여 항목</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">커뮤니티</a>
+                    <ul>
+                        <li><a href="#">병원소식</a></li>
+                        <li><a href="#">지료후기</a></li>
+                        <li><a href="#">전/후사진</a></li>
+                        <li><a href="#">건강정보</a></li>
+                        <li><a href="#">영상게시판</a></li>
+                        <li><a href="#">사회공헌사업</a></li>
+                        <li><a href="#">채용정보</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <p class="contact">
+                <a class="btn-tel" href="tel:15773622"><span class="glyphicon glyphicon-earphone"></span> 전화상담 <span class="bar" role="separator">|</span> 1577-3622</a>
+            </p>
+        </div>
+    </div>
+</nav>
