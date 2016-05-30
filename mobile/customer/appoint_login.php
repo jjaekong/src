d5d679b42e5506992f8ad5c29b4410461127c738
kyh5392 <kyh90495392@gmail.com>
2016-05-30 12:09:02 +0900

    고객 서비스 > 온라인예약 (로그인) 완료.

---
+++ mobile/customer/appoint_login.php
@@ -0,0 +1,62 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/customer.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+	<main id="content" class="customer appoint-login">
+		<div class="page-title">
+			<h2>온라인 예약</h2>
+		</div>
+		<div class="container">
+			<section class="online-reserve">
+				<ol class="row">
+					<li class="col-xs-3">
+						<img src="/mobile/images/customer/reserve1_on.png" alt="로그인"><br>
+						<h3>로그인</h3>
+					</li>
+					<li class="col-xs-3">
+						<img src="/mobile/images/customer/reserve2.png" alt="로그인"><br>
+						<h3>희망날짜, 의료진 및 진료시간 선택</h3>
+					</li>
+					<li class="col-xs-3">
+						<img src="/mobile/images/customer/reserve3.png" alt="로그인"><br>
+						<h3>예약 신청 내용 확인</h3>
+					</li>
+					<li class="col-xs-3">
+						<img src="/mobile/images/customer/reserve4.png" alt="로그인"><br>
+						<h3>완료 후 예약일에 방문</h3>
+					</li>
+				</ol>
+				<div class="section-header">
+					<h3>LOGIN</h3>
+					<p>SRC 재활병원 서비스를 이용하시려면<br> 로그인이 필요합니다.</p>
+				</div>
+				<div class="section-content">
+					<form class="login-input"> 
+						<div class="form-group">
+							<label class="sr-only" for="user-id">아이디</label>
+							<input type="text" class="form-control" id="user-id" placeholder="아이디를 입력해주세요.">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="user-password">비밀번호</label>
+							<input type="password" class="form-control" id="user-password" placeholder="비밀번호를 입력해주세요.">
+						</div>
+					</form>
+					<div class="btn-area">
+						<p>
+							<a href="#" class="btn btn-pink">로그인</a>
+						</p>
+					</div>
+					<ul>
+						<li><a href="#">회원가입</a></li>
+						<li><a href="#">아이디 / 비밀번호찾기</a></li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
+		
