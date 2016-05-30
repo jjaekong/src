7d8ca543b28c3de0cb4f3caffc7715831b1a6358
kyh5392 <kyh90495392@gmail.com>
2016-05-30 10:38:41 +0900

    고객서비스 > 제증명서 발급안내 완료.

--- mobile/customer/certificate.php
+++ mobile/customer/certificate.php
@@ -204,7 +204,182 @@
 					<h4>증명서 발급 절차안내 (외래환자)</h4>
 				</div>
 				<div class="section-content">
-					
+					<ol>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img1.png" alt="외래 접수">
+							<p>외래 접수</p>
+						</li>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img2.png" alt="주치의 상담">
+							<p>주치의 상담</p>
+						</li>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img3.png" alt="원무부 발급 신청">
+							<p>원무부 발급 신청</p>
+						</li>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img4.png" alt="수납 후 발행">
+							<p>수납 후 발행</p>
+						</li>
+					</ol>
+				</div>
+			</section>
+			<section class="inpatient">
+				<div class="section-header">
+					<h4>증명서 발급 절차안내 (입원환자)</h4>
+				</div>
+				<div class="section-content">
+					<ol>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img1.png" alt="외래 접수">
+							<p>외래 접수</p>
+						</li>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img4.png" alt="수납 후 발행">
+							<p>수납 후 발행</p>
+						</li>
+						<li>
+							<img src="/mobile/images/outpatient/medical_img5.png" alt="원무부 수령 (발급비용은 입원료에 포함)">
+							<p>원무부 수령<br>(발급비용은 입원료에 포함)</p>
+						</li>
+					</ol>
+				</div>
+			</section>
+			<section class="certificate-issue">
+				<div class="section-header">
+					<h4>증명서 발급 절차안내 (입원환자)</h4>
+				</div>
+				<div class="section-content">
+					<div class="table-wrap">
+						<table class="table">
+							<colgroup>
+								<col style="min-width:100px"/>
+								<col style="min-width:100px"/>
+								<col style="min-width:100px"/>
+							</colgroup>
+							<thead>
+								<th>종류</th>
+								<th colspan="2">비용</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td rowspan="2">일반진단서</td>
+									<td>한글</td>
+									<td>20,000</td>
+								</tr>
+								<tr>
+									<td>영문</td>
+									<td>40,000</td>
+								</tr>
+								<tr>
+									<td>소견서</td>
+									<td colspan="2">10,000</td>
+								</tr>
+								<tr>
+									<td>입통원확인서</td>
+									<td colspan="2">3,000</td>
+								</tr>
+								<tr>
+									<td>진료의뢰서</td>
+									<td colspan="2">비용없음</td>
+								</tr>
+								<tr>
+									<td rowspan="2">후유장애진단서</td>
+									<td>원본</td>
+									<td>150,000</td>
+								</tr>
+								<tr>
+									<td>부본</td>
+									<td>15,000</td>
+								</tr>
+								<tr>
+									<td rowspan="3">장애진단서</td>
+									<td>지체장애</td>
+									<td>-</td>
+								</tr>
+								<tr>
+									<td>정신장애</td>
+									<td>40,000</td>
+								</tr>
+								<tr>
+									<td>언어장애</td>
+									<td>40,000</td>
+								</tr>
+								<tr>
+									<td>보장구처방전</td>
+									<td colspan="2">비용없음</td>
+								</tr>
+								<tr>
+									<td>보장구검수확인서</td>
+									<td colspan="2">비용없음</td>
+								</tr>
+								<tr>
+									<td>건강진단서</td>
+									<td colspan="2">20,000</td>
+								</tr>
+								<tr>
+									<td rowspan="2">장애진단서</td>
+									<td>원본</td>
+									<td>100,000</td>
+								</tr>
+								<tr>
+									<td>부본</td>
+									<td>10,000</td>
+								</tr>
+								<tr>
+									<td rowspan="2">의무기록<br>복사비용</td>
+									<td>1~5장<br><br>6장 이상</td>
+									<td>2,000<br><br>장당 200원</td>
+								</tr>
+								<tr>
+									<td>CD 복사</td>
+									<td colspan="2">10,000</td>
+								</tr>
+								<tr>
+									<td rowspan="2">상해진단서</td>
+									<td>3주 이하</td>
+									<td>10,000</td>
+								</tr>
+								<tr>
+									<td>3주 이상</td>
+									<td>20,000</td>
+								</tr>
+								<tr>
+									<td>진단확인서<br>(보험회사제출용)</td>
+									<td colspan="2">20,000</td>
+								</tr>
+								<tr>
+									<td>국민연금<br>지체장애용 소견서</td>
+									<td colspan="2">30,000</td>
+								</tr>
+								<tr>
+									<td>국민연금<br>(소견서+장애진단서)</td>
+									<td colspan="2">40,000</td>
+								</tr>
+								<tr>
+									<td>근로능력평가용<br>진단서</td>
+									<td colspan="2">20,000</td>
+								</tr>
+								<tr>
+									<td>회송소견서</td>
+									<td colspan="2">10,000</td>
+								</tr>
+								<tr>
+									<td>부본(장당)</td>
+									<td colspan="2">2,000</td>
+								</tr>
+								<tr>
+									<td rowspan="2">향후치료비<br>추정서</td>
+									<td>천만원 이상</td>
+									<td>200,000</td>
+								</tr>
+								<tr>
+									<td>3천만원 이하</td>
+									<td>100,000</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</section>
 		</div>
@@ -265,8 +440,130 @@
 
 .customer.certificate .outpatients .section-header h4 {background:none !important; padding:0 !important; }
 
+.customer.certificate .outpatients .section-content ol {overflow:hidden; width:100%; margin-bottom:60px; }
+.customer.certificate .outpatients .section-content ol li {width:50%; text-align:center; /*height:170px;*/ display:inline-block; padding:0 23px 20px 23px; vertical-align:top; }
+.customer.certificate .outpatients .section-content ol li p {margin-top:10px; }
+
+.customer.certificate .outpatients .section-content ol li img {width:100px; height:100px;  }
+.customer.certificate .outpatients .section-content ol li:nth-of-type(1) {
+	position:relative;
+	float:left;
+}
+.customer.certificate .outpatients .section-content ol li:nth-of-type(1):after {
+	position:absolute; 
+	top:33px;
+	right:-9px;
+	display:block;
+	content:url("/mobile/images/outpatient/right_arrow.png");
+	transform:scale(.4);
+}
+
+.customer.certificate .outpatients .section-content ol li:nth-of-type(2) {
+	position:relative;
+	float:right;
+	
+}
+.customer.certificate .outpatients .section-content ol li:nth-of-type(2):after {
+	position:absolute; 
+	bottom:-13px;
+	display:inline-block;
+	text-align:center;
+	margin-left:-17px;
+	content:url("/mobile/images/outpatient/bottom_arrow.png");
+	transform:scale(.4);
+}
+
+.customer.certificate .outpatients .section-content ol li:nth-of-type(3) {
+	padding-top:20px;
+	position:relative;
+	float:right; 
+}
+.customer.certificate .outpatients .section-content ol li:nth-of-type(3):after {
+	position:absolute;
+	top:53px;
+	left:-9px;
+	display:block;
+	content:url("/mobile/images/outpatient/left_arrow.png");
+	transform:scale(.4);
+}
+.customer.certificate .outpatients .section-content ol li:nth-of-type(4) {
+	float:left; 
+	padding-top:20px;
+}
 
 
+.customer.certificate .inpatient .section-header h4 {background:none !important; padding:0 !important; }
+.customer.certificate .inpatient .section-content ol {overflow:hidden; width:100%; margin-bottom:60px; }
+.customer.certificate .inpatient .section-content ol li {width:50%; text-align:center; /*height:170px;*/ display:inline-block; padding:0 23px 20px 23px; vertical-align:top; }
+.customer.certificate .inpatient .section-content ol li p {margin-top:10px; }
+
+.customer.certificate .inpatient .section-content ol li img {width:100px; height:100px; }
+.customer.certificate .inpatient .section-content ol li:nth-of-type(1) {
+	position:relative;
+	float:left;
+}
+.customer.certificate .inpatient .section-content ol li:nth-of-type(1):after {
+	position:absolute; 
+	top:33px;
+	right:-9px;
+	display:block;
+	content:url("/mobile/images/outpatient/right_arrow.png");
+	transform:scale(.4);
+}
+
+.customer.certificate .inpatient .section-content ol li:nth-of-type(2) {
+	position:relative;
+	float:right;
+	
+}
+.customer.certificate .inpatient .section-content ol li:nth-of-type(2):after {
+	position:absolute; 
+	bottom:-13px;
+	display:inline-block;
+	text-align:center;
+	margin-left:-17px;
+	content:url("/mobile/images/outpatient/bottom_arrow.png");
+	transform:scale(.4);
+}
+
+.customer.certificate .inpatient .section-content ol li:nth-of-type(3) {
+	padding-top:20px;
+	float:right; 
+}
+
+.customer.certificate .certificate-issue .section-header h4 {background:none !important; padding:0 !important; }
+.customer.certificate .certificate-issue .section-content .table-wrap {background:none !important; padding:0 !important; table-layout:fixed; margin:30px 0 60px 0; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table thead {border-left:#4f4f4f; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table thead tr th {background-color:#4f4f4f; color:#fff; font-size:13px; border-bottom:0; border-right:1px solid #d7d7d7; text-align:center; font-weight:300; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table thead tr th:nth-of-type(2) {border-right:1px solid #4f4f4f; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr td {border-left:1px solid #d7d7d7; border-right:1px solid #d7d7d7; text-align:center; font-size:12px; color:#666; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table thead tr {height:41px; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr {height:41px; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr td {height:41px; vertical-align:middle; }
+
+
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(1) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(3) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(4) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(5) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(6) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(8) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(11) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(12) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(13) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(14) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(16) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(18) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(20) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(21) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(22) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(23) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(24) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(25) td:first-child {background-color:#f9f9f9; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(26) td:first-child {background-color:#f9f9f9; }
+
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(26) {border-bottom:1px solid #d7d7d7; }
+.customer.certificate .certificate-issue .section-content .table-wrap .table tbody tr:nth-of-type(27) {border-bottom:1px solid #d7d7d7; }
 
 
 
