c8b322820fcedf39e66cd966478169ad9600f7db
kyh5392 <kyh90495392@gmail.com>
2016-07-12 16:03:18 +0900

    [web] 입원(재활센터) > 뇌졸증 재활센터, 척추손상 재활센터, 호흡 재활센터, 소아 재활센터, 척추관절재활센터, 암요양병원 완료 #1

---
+++ inpatient/joint.php
@@ -0,0 +1,49 @@
+ <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+<link href="/css/inpatient.css" rel="stylesheet">
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <div class="page-path">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li><a href="/"><span class="glyphicon glyphicon-home"></span></a></li>
+                <li><a href="#">입원안내</a></li>
+                <li class="active">척추관절재활센터</li>
+            </ol>
+        </div>
+    </div>
+    <main id="content" class="inpatient joint"><!-- 클래스명은 대메뉴 > 서브메뉴명의 방식으로 -->
+        <div class="page-header"><!-- 배경은 위의 클래스명을 이용하여 -->
+            <h2>척추관절재활센터</h2>
+        </div>
+        <!-- 실제 작업 영역 -->
+        <div class="container">
+            <section class="joint-center">
+                <div class="section-header"> 
+                    <h2>소아재활센터 소개</h2>
+                </div>
+                <p>
+                    흔히 우리 몸에 통증이 있을 때 신체검사 후에 엑스레이와 초음파, 혹은 CT, MRI 등의 검사를 진행하고, 물리치료나 주사치료, 심할 경우 수술을 실시하는 것이 일반적입니다. 
+                </p>
+                <p>
+                    그러나 이러한 치료를 통해 통증이 사라진다고 해도, 근본적인 원인까지 해결됐을지는 확답할 수 없는 상황임에도 불구하고 통증 치료 시에 환자들, 심지어 의료인들마저도 통증을 일으키는 부위만 치료하는데 그치는 경우가 대부분입니다. 통증을 느끼게 되면 그 상황을 최대한 빨리 밝혀내고, 본질적인 원인을 찾아서 해결하는 것이 중요합니다. 근육 및 뼈의 만성 통증은 대부분 특정 부위의 과도한 반복 사용으로 인한 질환이 원인이며 또한 해당 부위에 압력이 불균형하게 가해지거나, 이 부위를 안정화시켜 주는 다른 구조에 문제가 생기면서 발생할 수도 있습니다. 때문에 환자의 신체 상태, 자세, 평소의 생활습관까지 종합적으로 확인해서 근본적인 원인을 해결해야, 같은 질환의 재발 및 악화를 막을 수 있습니다. 
+                </p>
+                <p>
+                    SRC 재활병원의 척추 관절 재활 센터에서는 척추와 관절의 통증에 대한 비수술적 치료를 목표로 하고 있습니다. 정확한 진단을 위한 X ray, CT, 근골격계 초음파 등의 검사와 함께 통증의 완화를 위한 주사치료 및 체외충격파치료 등을 시행하고 있습니다. 병원 내에 체형분석, 족저압 분석, 보행 분석을 위한 완벽한 시스템이 갖추어져 있으며 이를 통한 통증의 원인분석을 시행하고 있습니다. 통증의 원인에 따른 환자 맞춤형의 치료를 시행하여 뼈와 관절의 정렬 이상 및 이탈구 등을 해결하고, 이완도수치료 및 강화도수치료를 통하여 특정 근육과 힘줄, 인대 구조의 단축과 변형 등의 문제를 해결하고 척추 및 관절 안정화 근육들을 강화하여 통증의 원인을 근본적으로 해결하고자 하고 있습니다. 또한 자연적으로는 잘 회복되지 않는 인대, 힘줄 그리고 연골의 손상에 대해서는 DNA 주사 및 프롤로 요법 등을 포함한 재생치료를 시행하여 수술 없이 통증의 근본원인 해결이 이루어지도록 하고 있습니다. 통증으로 고생하는 모든 분들이 아프지 않는 그날까지 저희 SRC 재활병원 척추 관절 재활 센터가 노력하겠습니다.
+                </p>
+                <p>
+                    감사합니다.
+                </p>
+            </section>
+            <section class="postscript">
+                <div class="section-header">
+                    <h2>입원치료 후기</h2>
+                </div>
+            </section>
+        </div>
+        <!-- // 실제 작업 영역 -->
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer_sub.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ inpatient/young.php
@@ -0,0 +1,105 @@
+ <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+<link href="/css/inpatient.css" rel="stylesheet">
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <div class="page-path">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li><a href="/"><span class="glyphicon glyphicon-home"></span></a></li>
+                <li><a href="#">입원안내</a></li>
+                <li class="active">소아 재활센터</li>
+            </ol>
+        </div>
+    </div>
+    <main id="content" class="inpatient young"><!-- 클래스명은 대메뉴 > 서브메뉴명의 방식으로 -->
+        <div class="page-header"><!-- 배경은 위의 클래스명을 이용하여 -->
+            <h2>소아 재활센터</h2>
+        </div>
+        <!-- 실제 작업 영역 -->
+        <div class="container">
+            <section class="intro">
+                <div class="section-header"> 
+                    <h2>소아재활센터 소개</h2>
+                </div>
+                <h3>소아재활치료의 중요성</h3>
+                <ul class="arrow-list">
+                    <li>
+                        성장과 발달이 완성되지 않은 시기에 질환 혹은 사고 등의 여러 가지 원인에 의하여 장애가 있는 아동을 대상으로 장애를 최소화 하고 연령에 맞는 발달을 할 수 있도록 도와주는 재활치료를 시행하고 있습니다. 대상이 되는 질환은 뇌성마비, 외상성뇌손상, 척수이형성증, 근육이영양증 이외 기타 신경학적, 정형외과적 문제들과 여러 가지 원인에 의하여 발달이 지연된 경우입니다. 장애를 가진 소아의 재활치료는 진단과 치료의 접근이 성인보다 복잡하고 어렵기 때문에 각별한 관심과 이해가 필요하며 전문적인 의료진의 역할이 매우 중요합니다. 
+                    </li>
+                </ul>
+                <h3>소아재활 낮병동이란?</h3>
+                <ul class="arrow-list">
+                    <li>
+                        낮시간 동안 입원하여 재활치료를 받고, 저녁에는 가정으로 돌아가 생활하는 형태로, 집중적인 재활치료 프로그램을 통한 치료 효과 증대 및 가족 유대감 강화와 정서적인 안정 효과를 기대할 수 있는 장점이 있습니다. 
+                    </li>
+                    <li>
+                        재활치료 프로그램에는 재활의학과 전문의, 재활간호사, 물리치료사, 작업치료사, 언어치료사 등 각 분야의 전문가들이 아동에 맞는 1:1 치료를 시행하게 되어 장애 개선에  큰 도움을 줄 수 있습니다.
+                    </li>
+                </ul>
+            </section>
+            <section class="advantage">
+                <div class="section-header">
+                    <h2>SRC병원 소아 낮병동만의 장점</h2>
+                </div>
+                <ul>
+                    <li>SRC재활병원의<br>60년 재활전문의료<br>노하우 보유</li>
+                    <li>쾌적하고 이용이 편리한<br>입원 및 치료 환경 제공</li>
+                    <li>소아식/일반식/<br>연하장애식 등<br>맞춤형 식단 제공</li>
+                    <li>
+                        SRC언어심리상담센터 운영<br>
+                        <small>(놀이, 인지, 미술, 심리 외<br>기타 치료 가능.)</small>
+                    </li>
+                    <li>SRC스포츠센터<br>(수영장) 운영.<br>수중운동 연계 가능</li>
+                </ul>
+            </section>
+            <section class="seoul-center">
+                <div class="section-header">
+                    <h2>SRC서울센터에서도 수준높은 소아재활서비스를 만나실 수 있습니다.</h2>
+                </div>
+                <div class="section-content">
+                    <h3>SRC서울센터 소아재활 낮병동</h3>
+                    <ul class="arrow-list">
+                        <li>
+                            <dl class="inline-dl">
+                                <dt>대표전화</dt>
+                                <dd> 02-871-3651</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl class="inline-dl">
+                                <dt>FAX</dt>
+                                <dd> 02-876-8234</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <h3>외래진료</h3>
+                    <ul class="arrow-list">
+                        <li>
+                            <dl class="inline-dl">
+                                <dt>대표전화</dt>
+                                <dd> 02-876-3633 </dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl class="inline-dl">
+                                <dt>FAX</dt>
+                                <dd> 02-872-3653</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <h3>웹사이트</h3>
+                    <ul class="arrow-list">
+                        <li>
+                            <a href="http://www.seoulsrc.com" target="_blank">http://www.seoulsrc.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- // 실제 작업 영역 -->
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer_sub.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
