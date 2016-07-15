0c913c4ebb0fc472a25e16a73d29c5705087f9c3
kyh5392 <kyh90495392@gmail.com>
2016-07-15 19:04:15 +0900

    [web]  병원소개 > 의료진소개 modal 수정 #2

--- introduce/medical_team.php
+++ introduce/medical_team.php
@@ -27,7 +27,7 @@
                         <h3>재활병원 의료진</h3>
                         <ul class="team-list">
                             <li>
-                                <a href="#">
+                                <a href="#" data-toggle="modal" data-target="#staff-modal" >
                                     <figure>
                                         <p>
                                             <img src="/images/introduce/staff_img01.png" alt="">
@@ -231,46 +231,48 @@
         <!-- // 실제 작업 영역 -->
     </main>
 
-    <!-- The Modal -->
-    <div id="myModal" class="modal medical-team">
-    <!-- Modal content -->
-        <div class="staff-content">
-            <button type="button" class="close" data-dismiss="modal">
-                <img src="/images/introduce/close.png" alt="닫기">
-            </button>
-
-            <h1>의료진 소개</h1>
-            <figure>
-                <img src="/images/introduce/modal_img01.jpg" alt="김형빈 부장">
-                <figcaption>
-                    <dl>
-                        <dt>김형빈 부장</dt>
-                        <dd>재활의학과 전문의</dd>
-                    </dl>
-                </figcaption>
-            </figure>
-            <div class="staff-history">
-                <ul class="arrow-list">
-                    <li>SRC 요양병원 부원장</li>
-                    <li>연세대학교 의과대학 재활의학과 외래교수</li>
-                    <li>SRC 재활의학 연구소장</li>
-                    <li>2015년 분당서울대학교병원 2년 연송 베스트 파트너 선정</li>
-                    <li>2014년 인천 장애인 아시안게임 조직위원회 전문위원</li>
-                    <li>이천 장애인 체육종합 훈련원 주치의</li>
-                    <li>대한 스포츠의학회 장애인 스포츠 분과위원</li>
-                    <li>2012년 런던 장애인 올림픽 대표팀 주치의</li>
-                    <li>2010년 광저우 장애인 아시안게임 대표팀 주치의</li>
-                    <li>미국의사수련면허 (ECFMG CERTIFICATION) 보유</li>
-                    <li>2005년 세브란스 베스트닥터 상 수상</li>
-                    <li>대한 재활의학회 정회원</li>
-                    <li>대한 스포츠의학회 정회원</li>
-                    <li>대한 신경근골격 초음파학회 정회원</li>
-                    <li>대한 심장호흡재활학회 정회원</li>
-                    <li>대한 근전도 전기진단학회 정회원</li>
-                    <li>대한 뇌신경재활학회 정회원</li>
-                    <li>신촌 세브란스병원 재활의학과 전공의 수료</li>
-                    <li>연세대학교 의과대학 졸업</li>
-                </ul>
+    <!-- Modal -->
+    <div class="modal fade" id="staff-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span><img src="/images/introduce/close.png" alt="닫기"></span>
+                    </button>
+                    <h1>의료진 소개</h1>
+                </div>
+                <figure>
+                    <img src="/images/introduce/modal_img01.jpg" alt="김형빈 부장">
+                    <figcaption>
+                        <dl>
+                            <dt>김형빈 부장</dt>
+                            <dd>재활의학과 전문의</dd>
+                        </dl>
+                    </figcaption>
+                </figure>
+                <div class="staff-history">
+                    <ul class="arrow-list">
+                        <li>SRC 요양병원 부원장</li>
+                        <li>연세대학교 의과대학 재활의학과 외래교수</li>
+                        <li>SRC 재활의학 연구소장</li>
+                        <li>2015년 분당서울대학교병원 2년 연송 베스트 파트너 선정</li>
+                        <li>2014년 인천 장애인 아시안게임 조직위원회 전문위원</li>
+                        <li>이천 장애인 체육종합 훈련원 주치의</li>
+                        <li>대한 스포츠의학회 장애인 스포츠 분과위원</li>
+                        <li>2012년 런던 장애인 올림픽 대표팀 주치의</li>
+                        <li>2010년 광저우 장애인 아시안게임 대표팀 주치의</li>
+                        <li>미국의사수련면허 (ECFMG CERTIFICATION) 보유</li>
+                        <li>2005년 세브란스 베스트닥터 상 수상</li>
+                        <li>대한 재활의학회 정회원</li>
+                        <li>대한 스포츠의학회 정회원</li>
+                        <li>대한 신경근골격 초음파학회 정회원</li>
+                        <li>대한 심장호흡재활학회 정회원</li>
+                        <li>대한 근전도 전기진단학회 정회원</li>
+                        <li>대한 뇌신경재활학회 정회원</li>
+                        <li>신촌 세브란스병원 재활의학과 전공의 수료</li>
+                        <li>연세대학교 의과대학 졸업</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
